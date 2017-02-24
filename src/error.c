5feb73c0a1f44023ef2b7b271f6036ac533d0ab3
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2017-02-24 13:21:25 -0500

    add in fixes for upstream bug reports

--- src/error.c
+++ src/error.c
@@ -137,6 +137,9 @@ static const char* find_last_newline(
     const char* original_text, const char* error_location) {
   assert(error_location >= original_text);
   const char* c = error_location;
+  // if the error location itself is a newline then start searching for 
+  // the preceding newline one character earlier
+  if (*error_location == '\n') --c;
   for (; c != original_text && *c != '\n'; --c) {
     // There may be an error at EOF, which would be a nul byte.
     assert(*c || c == error_location);
